90ccae14cf62fc3d0de92aae496a2595ec6f5eb8
tikrong <tikrong@mail.ru>
2021-02-05 22:29:54 +1000

    Added chart and statistics

--- virus.py
+++ virus.py
@@ -2,27 +2,33 @@ import random
 import time
 import copy
 import pygame
+import numpy as np
 
 pygame.init()
 
+#fonts
+def_font = pygame.font.SysFont("verdana", 24)
+small_font = pygame.font.SysFont("verdana", 16)
+
 #colors
 WHITE = (255,255,255)
-BLACK = (255,255,255)
+BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 #screen size
-screen_height = 770
-screen_width = 770
+screen_height = 800
+screen_width = 1400
 
 # sizes
-tile_size = 10
+tile_size = 7
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # generate a 2d array of people 5x5
-population = [[0 for n in range(70)] for x in range(70)]
+#population = [[0 for n in range(100)] for x in range(100)]
+population = np.zeros((100,100))
 
 # status of people
 status = [0,1,2,3]
@@ -31,13 +37,20 @@ status[1] = "immune"
 status[2] = "ill"
 status[3] = "dead"
 
+infected_people = 0
+immune_people = 0
+healthy_people = 0
+dead_people = 0
+
+days = 0
+
 # probabilities
-prob_death = 0.1
-prob_recover = 0.3
-contagiousness = 0.05
+prob_death = 0.01
+prob_recover = 0.1
+contagiousness = 0.35
 
 # randomly infect several people in population
-i = random.randint(1,15) # number of infected
+i = random.randint(1,2) # number of infected
 
 for k in range(i):
     population[random.randint(0, len(population)-1)][random.randint(0,len(population)-1)] = 2
@@ -69,6 +82,105 @@ def get_sick_prob(y, x):
 
 running = True
 while running:
+    screen.fill(BLACK,(850, 60, 300, 200))
+    #count the amount of sick, healthy, immune and dead
+    (status, counts) = np.unique(population, return_counts = True)
+    people_status = np.asarray((status, counts)).T
+    for i in range(len(people_status)):
+        if people_status[i][0] == 0:
+            healthy_people = people_status[i][1]
+        elif people_status[i][0] == 1:
+            immune_people = people_status[i][1]
+        elif people_status[i][0] == 2:
+            infected_people = people_status[i][1]
+        elif people_status[i][0] == 3:
+            dead_people = people_status[i][1]
+        
+    
+    #draw UI
+    #name of project
+    rect = pygame.Rect(850, 20, 50, 100)
+    program_name = def_font.render("DISEASE SIMULATION", False, WHITE)
+    screen.blit(program_name, rect)
+
+    #number of infected
+    infected_num_text = def_font.render("Infected:", False, WHITE)
+    infected_num_text_rect = infected_num_text.get_rect()
+    infected_num_text_rect.move_ip(850,60)
+    screen.blit(infected_num_text, infected_num_text_rect)
+    infected_num = def_font.render(str(infected_people), False, WHITE)
+    infected_num_rect = infected_num.get_rect()
+    infected_num_rect.move_ip(infected_num_text_rect.right + 10, infected_num_text_rect.top)
+    screen.blit(infected_num, infected_num_rect)
+
+    #number of immune
+    immune_num_text = def_font.render("Immune:", False, WHITE)
+    immune_num_text_rect = immune_num_text.get_rect()
+    immune_num_text_rect.move_ip(infected_num_text_rect.left, infected_num_rect.bottom+10)
+    screen.blit(immune_num_text, immune_num_text_rect)
+    immune_num = def_font.render(str(immune_people), False, WHITE)
+    immune_num_rect = immune_num.get_rect()
+    immune_num_rect.move_ip(immune_num_text_rect.right + 10, immune_num_text_rect.top)
+    screen.blit(immune_num, immune_num_rect)
+
+    #number of dead
+    dead_num_text = def_font.render("Dead:", False, WHITE)
+    dead_num_text_rect = dead_num_text.get_rect()
+    dead_num_text_rect.move_ip(immune_num_text_rect.left, immune_num_rect.bottom+10)
+    screen.blit(dead_num_text, dead_num_text_rect)
+    dead_num = def_font.render(str(dead_people), False, WHITE)
+    dead_num_rect = dead_num.get_rect()
+    dead_num_rect.move_ip(dead_num_text_rect.right + 10, dead_num_text_rect.top)
+    screen.blit(dead_num, dead_num_rect)
+
+    #number of dead
+    healthy_num_text = def_font.render("Healthy:", False, WHITE)
+    healthy_num_text_rect = healthy_num_text.get_rect()
+    healthy_num_text_rect.move_ip(dead_num_text_rect.left, dead_num_rect.bottom+10)
+    screen.blit(healthy_num_text, healthy_num_text_rect)
+    healthy_num = def_font.render(str(healthy_people), False, WHITE)
+    healthy_num_rect = healthy_num.get_rect()
+    healthy_num_rect.move_ip(healthy_num_text_rect.right + 10, healthy_num_text_rect.top)
+    screen.blit(healthy_num, healthy_num_rect)
+
+    #number of days passed
+    days_num_text = def_font.render("Days:", False, WHITE)
+    days_num_text_rect = days_num_text.get_rect()
+    days_num_text_rect.move_ip(healthy_num_text_rect.left, healthy_num_rect.bottom+10)
+    screen.blit(days_num_text, days_num_text_rect)
+    days_num = def_font.render(str(days), False, WHITE)
+    days_num_rect = days_num.get_rect()
+    days_num_rect.move_ip(days_num_text_rect.right + 10, days_num_text_rect.top)
+    screen.blit(days_num, days_num_rect)
+
+    #CHART
+    # draw an area for future chart
+    if days <= 1:
+        chart_immune = small_font.render("Share of immune", False, WHITE)
+        chart_immune_rect = chart_immune.get_rect()
+        chart_immune_rect.move_ip(days_num_text_rect.left, days_num_text_rect.bottom+15)
+        screen.blit(chart_immune, chart_immune_rect)
+        chart_area = pygame.Rect(chart_immune_rect.left, chart_immune_rect.bottom+5, 300, 100)
+        pygame.draw.rect(screen, WHITE, chart_area)
+        chart_infected = small_font.render("Share of infected", False, WHITE)
+        chart_infected_rect = chart_infected.get_rect()
+        chart_infected_rect.move_ip(chart_area.left, chart_area.bottom+5)
+        screen.blit(chart_infected, chart_infected_rect)
+
+    
+    #draw a line of infected
+    share_of_infected = (infected_people / 10000) * 100
+    infected_dot = pygame.Rect(chart_area.left + days, chart_area.bottom - share_of_infected, 1,1)
+    pygame.draw.rect(screen, BLACK, infected_dot)
+
+    #draw a line for immune
+    share_of_immune = (immune_people / 10000) * 100
+    immune_dot = pygame.Rect(chart_area.left + days, chart_area.top + share_of_immune, 1,1)
+    pygame.draw.rect(screen, BLACK, immune_dot)
+
+
+
+    
     #draw array of people
     for y in range(len(population)):
         for x in range(len(population)):
@@ -97,8 +209,10 @@ while running:
                 tmp_population[y][x] = random.choices([1,3,2], weights=(prob_recover, prob_death, 1-prob_recover-prob_death), k=1)[0]
 
     population = copy.deepcopy(tmp_population)
+    #add new day at the end of the cycle
+    days += 1
     pygame.display.flip()
-    time.sleep(0.1)
+    #time.sleep(0.1)
 
 
             
